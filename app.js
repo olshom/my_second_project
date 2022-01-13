afd91dcd75599c7ecf605b5bef2e9f7e429ece1d
Olga Shomarova <olga.shomarova@protonmail.com>
2022-01-13 13:34:34 +0200

    fix import

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { Application, Session } from "./deps.js";
 import { authMiddleware } from "./middlewares/authMiddleware.js";
 import { errorMiddleware } from "./middlewares/errorMiddleware.js";
-import {renderMiddleware} from "./middlewares/renderMiddleware.js";
+import renderMiddleware from "./middlewares/renderMiddleware.js";
 import { serveStaticMiddleware } from "./middlewares/serveStaticMiddleware.js";
 import { router } from "./routes/routes.js";
 import { api } from "./routes/apis/quizApi.js"
